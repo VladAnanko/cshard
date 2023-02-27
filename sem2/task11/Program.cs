4818f65aaddbf86497021a249b2f7a6c2dd40c4b
Vladislav Ananko <anankogb@gmail.com>
2023-02-27 22:02:25 +0300

    sem2 end

---
+++ sem2/task11/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+int arg = Random.Shared.Next(100, 1000);
+int arg1 = arg / 100;
+int arg2 = arg % 10;
+int argnew = arg1 * 10 + arg2;
+Console.WriteLine(arg);
+Console.WriteLine(argnew);
---
+++ sem2/task12/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. 
+// Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+
+// 34, 5 -> не кратно, остаток 4 
+// 16, 4 -> кратно
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+while (a == 0) 
+    {
+    Console.Write("Введите другое число: ");
+    a = Convert.ToInt32(Console.ReadLine());
+    }
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int numb = b % a;
+if (numb == 0) 
+{
+    Console.WriteLine("второе число кратно первому");
+}
+else 
+{
+    Console.WriteLine("Остаток от деления равен :" + numb);
+}
